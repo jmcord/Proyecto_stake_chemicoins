60be32b03d32b3889ee86183b9cbd01af0f47c4d
jmcord <jcorderodz@hotmail.com>
2024-03-08 11:38:46 +0100

    c

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,14 @@
-import { ConnectKitButton } from "connectkit"
+import React from 'react';
+import { ConnectKitButton } from "connectkit";
 
 export default function Header(){
     return (
-        <header className="flex px-8 py-4 border-b justify-between items-center
-        bg-white">
+        <header className="bg-cover bg-center bg-no-repeat" style={{backgroundImage: 'url(/header.png)'}}>
+            <div className="flex px-8 py-4 border-b justify-between items-center bg-white">
                 <img src="bm.png" alt="blockmaker-logo" className="sm:hidden" width={50}/>
                 <img src="ADA_description.png" alt="blockmaker-full-logo" className="hidden sm:flex" width={200}/>
-
                 <ConnectKitButton showBalance />
-         
+            </div>
         </header>
     )
 }
