a3e618b5e5d232e58276605a323fb7ea1df2bc99
jmcord <jcorderodz@hotmail.com>
2024-03-15 19:32:02 +0100

    back image

--- src/components/ui/layouts/AppLayout.jsx
+++ src/components/ui/layouts/AppLayout.jsx
@@ -1,14 +1,23 @@
-import Header from '../../Header'
-import Footer from '../../Footer'
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types'; // Importa PropTypes desde la biblioteca prop-types
+import Header from '../../Header';
+import Footer from '../../Footer';
+import backgroundImage from '/public/bc.png'; // Importa la imagen de fondo
 
-
-export default function AppLayout({children}){
-    return (
-        <>
-          <Header/>
-          <main className='min-h-[85vh]'>{children}</main>
-          <Footer />
-        </>
-      )
+export default function AppLayout({ children }) {
+  return (
+    <>
+      <Header />
+      <main
+        className='min-h-[85vh]'
+        style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat' }}
+      >
+        {children}
+      </main>
+      <Footer />
+    </>
+  );
 }
+
+AppLayout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
